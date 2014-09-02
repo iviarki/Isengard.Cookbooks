f287ee63397e90e3e4d6679a8d2bdc4fdefa5218
Mark Tomzynski <tomzynsk@amazon.com>
2014-09-02 08:07:15 +0200

    Fixed logging

--- isengard-kafka/recipes/test.rb
+++ isengard-kafka/recipes/test.rb
@@ -1,7 +1,9 @@
 # get by opsworks layers (http://docs.aws.amazon.com/opsworks/latest/userguide/attributes-json-opsworks-layers.html)
+Chef::Log.info("Running Test Recipe")
 zk_hosts = Array.new
 node[:opsworks][:layers]['zookeeper'][:instances].each do |k,v|
 # TODO: filter out :status != 'online'
 Chef::Log.debug(v[:private_ip])
+Chef::Log.info(v[:private_ip])
 zk_hosts << v
 end
